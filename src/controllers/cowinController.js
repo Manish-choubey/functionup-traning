cac865a56a25b5021b84b68d9bdabf1a590b2be9
Manish-choubey <109017549+Manish-choubey@users.noreply.github.com>
2022-09-02 11:19:00 +0530

    chenge

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,5 @@
-let axios = require("axios")
+let axios = require("axios");
+//const { post } = require("../routes/route");
 
 
 let getStates = async function (req, res) {
@@ -79,6 +80,52 @@ let getOtp = async function (req, res) {
 }
 
 
+
+
+const getvaccination= async function(req,res){
+      let _id = req.query.district_Id
+      let date = req.query.date
+      let options ={
+        method:"get",
+        url:`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=31-03-2021`,
+
+      }
+      let result = await axios(options)
+      //console.log(result.data)
+      res.send({msg:result.data})
+}
+
+
+const getshortedcity = async function (req,res){
+    let cities = ["Bengaluru","Mumbai","Delhi","kolkata","Chennai","London","Moscow"]
+    let citiObjArray=[]
+    for(i=0;i<cities.length;i++){
+        let obj= {city:cities[i]}
+        let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=1cbefa27a8ad48b3137cf353808db0ea`)
+        console.log(resp.data.main.temp)
+        obj.temp=resp.data.main.temp
+        citiObjArray.push(obj)
+    }
+    let sorted = citiObjArray.sort(function(a,b){return a.temp-b.temp})
+    console.log(sorted)
+    res.send({status:true,data:sorted})
+}
+
+
+
+const creatememe =async function(req,res){
+    let options={
+        method:"post",
+        url:("https://api.imgflip.com/caption_image?temlate_id=178591752&text0=learningcoding&text1=hardthing&username=chewie12345&password=meme@123")
+    }
+    let result = await axios(options)
+    res.send({data:result.data})
+}
+
+module.exports.getshortedcity=getshortedcity
+module.exports.creatememe =creatememe 
+
+module.exports.getvaccination=getvaccination
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,11 @@ router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
+router.get("/cowin/getbydistric", CowinController.getvaccination)
+
+router.get("/getcititem",CowinController.getshortedcity)
+router.post("/creatimg",CowinController.creatememe)
+
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
